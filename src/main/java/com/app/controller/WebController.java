35f2a489c91f2b9faa6aa63706fcac6f6606a895
1012pos <1012pos@naver.com>
2024-07-26 10:59:55 +0900

    JDBC 아이피 변경

--- src/main/java/com/app/controller/WebController.java
+++ src/main/java/com/app/controller/WebController.java
@@ -1,5 +1,6 @@
 package com.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebController {
     
+	
+	@Autowired
+	private TestMapper tm;
+	
     @GetMapping("/")
     public String home(){
-        return "환영합니다.";
+        return "환영합니다 (" + tm.testDB() +")";
     }
 
 }
